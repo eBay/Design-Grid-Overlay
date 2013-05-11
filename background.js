6b93571dc66c9f267b15b784cbc951101dc3b876
Carl Henderson <chuckhendo@gmail.com>
2013-05-11 14:50:36 -0400

    Added icon toggle; multiple icons

--- background.js
+++ background.js
@@ -1,5 +1,17 @@
+var toggleIcons = ["icon32_on.png", "icon32_off.png"];
+var current = true;
+
+var getCurrentIcon = function(toggle) {
+  toggle && (current = !current);
+  var icon = toggleIcons[+current];
+  return icon;
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-  
+
+
+
+  chrome.browserAction.setIcon({path: getCurrentIcon(true)});  
   chrome.tabs.insertCSS({
     file: 'grid.css'
   });
--- grid.js
+++ grid.js
@@ -1,16 +1,24 @@
-document.body.innerHTML += '<div class="grid-lines"> \
-  <div class="container"> \
-    <div class="span1"></div> \
-    <div class="span1"></div> \
-    <div class="span1"></div> \
-    <div class="span1"></div> \
-    <div class="span1"></div> \
-    <div class="span1"></div> \
-    <div class="span1"></div> \
-    <div class="span1"></div> \
-    <div class="span1"></div> \
-    <div class="span1"></div> \
-    <div class="span1"></div> \
-    <div class="span1"></div> \
-  </div> \
-</div>'
+if (document.getElementsByClassName('cb-grid-lines').length){
+    
+    document.body.removeChild(document.getElementsByClassName('cb-grid-lines')[0]);
+}
+
+else {
+    document.body.innerHTML += '<div class="cb-grid-lines"> \
+      <div class="container"> \
+        <div class="span1"></div> \
+        <div class="span1"></div> \
+        <div class="span1"></div> \
+        <div class="span1"></div> \
+        <div class="span1"></div> \
+        <div class="span1"></div> \
+        <div class="span1"></div> \
+        <div class="span1"></div> \
+        <div class="span1"></div> \
+        <div class="span1"></div> \
+        <div class="span1"></div> \
+        <div class="span1"></div> \
+      </div> \
+    </div>';
+    
+}
