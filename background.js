f17e5ca713963e9c83a5823cb04d219d4906225d
atopper <atopper@ebay.com>
2015-07-14 16:46:17 -0700

    Initial playing around with having a grid overlay in Darkstar

--- background.js
+++ background.js
@@ -2,21 +2,21 @@ var toggleIcons = ["icon32_on.png", "icon32_off.png"];
 var current = true;
 
 var getCurrentIcon = function(toggle) {
-  toggle && (current = !current);
-  var icon = toggleIcons[+current];
-  return icon;
-}
+    toggle && (current = !current);
+    var icon = toggleIcons[+current];
+    return icon;
+};
+
+chrome.browserAction.setIcon({path: toggleIcons[1]});
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-
-
-
-  chrome.browserAction.setIcon({path: getCurrentIcon(true)});  
-  chrome.tabs.insertCSS({
-    file: 'grid.css'
-  });
-  chrome.tabs.executeScript({
-    file: 'grid.js'
-  });
-
+    chrome.browserAction.setIcon({path: getCurrentIcon(true)});  
+    
+    chrome.tabs.insertCSS({
+        file: 'grid.css'
+    });
+    
+    chrome.tabs.executeScript({
+        file: 'grid.js'
+    });
 });
--- grid.js
+++ grid.js
@@ -1,26 +1,38 @@
-if (document.getElementsByClassName('cb-grid-lines').length){
-    
+if (document.getElementsByClassName('cb-grid-lines').length){  
     document.body.removeChild(document.getElementsByClassName('cb-grid-lines')[0]);
-}
-
-else {
-    document.body.innerHTML += '<div class="cb-grid-lines"> \
-      <div class="container"> \
-        <div class="row"> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-        </div> \
+} else {
+    var numColumns = 12;
+    var columnSelector = '.grid-overlay-container .row > .span1';
+    var output = '<div class="cb-grid-lines"> \
+      <div class="grid-overlay-container"> \
+        <div class="row">';
+    for (var i = 0; i < numColumns; i += 1) {
+        output +=  '<div class="span1 col-xs-1"></div>';
+    }
+    output += '</div> \
       </div> \
     </div>';
+    document.body.innerHTML += output;
     
+    
+    //var gridOverlay = document.querySelectorAll('.grid-overlay-container');
+    var columns = document.querySelectorAll(columnSelector);
+    
+    /**
+    for (var j = 0; j < columns.length; j += 1) {
+        columns[j].style.width = 'calc('+(100 / numColumns)+'% - 16 px)';
+    }
+    */
+    var styleEl = document.createElement('style');
+
+    // Append style element to head
+    document.head.appendChild(styleEl);
+    
+    var dynamicGridRules = styleEl.sheet;
+    
+    console.log(dynamicGridRules);
+    console.log(100 / numColumns);
+    
+    //dynamicGridRules.insertRule(columnSelector + ' { color: #fff; width: calc('+(100 / numColumns)+'% - 16 px); }', 0);
+    //dynamicGridRules.insertRule('.grid-overlay-container .row > * { color: #fff; }', 1);
 }
