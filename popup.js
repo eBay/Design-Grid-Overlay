0423ceca4943d32a2957b582ace315d8aaa867fe
Norman <chnorman@lm-sjc-11000325.corp.ebay.com>
2015-08-05 17:15:11 -0700

    You can now dynamically change the size of your grid

--- popup.js
+++ popup.js
@@ -4,43 +4,67 @@ function addGrid(){
 	chrome.tabs.insertCSS({ 
         file: 'grid.css'
     }, function() {
-        chrome.tabs.executeScript(null, {
-            file: 'grid.js'
-        });    
+    	var columns = document.getElementById("columns").value;
+		var width = document.getElementById("width").value;
+
+		localStorage.setItem("width", width);
+    	localStorage.setItem("columns", columns);
+
+	    var options = {
+	    	width: width
+	    }
+
+        executeJS();
+        executeCSS(options);
     }); 
 }
 
 
 function upDateGrid(){
 
+	var columns = document.getElementById("columns").value;
+	var width = document.getElementById("width").value;
+    
+	localStorage.setItem("width", width);
+    localStorage.setItem("columns", columns);
 
+    var options = {
+    	width: width
+    }
 
+    removeGrid();
+    executeCSS(options);
+
+    addGrid();
 }
 
 
 function removeGrid(){
-	chrome.tabs.insertCSS({ 
-        file: 'grid.css'
-    }, function() {
-        chrome.tabs.executeScript(null, {
-            file: 'grid.js'
-        });    
-    }); 
-}
-
-function pullFromLocalStorage(){
-
-
-
-}
-
-function pushToLocalStorage(){
-
-
-
+	executeJS();
 }
 
 
+function executeCSS(options){
+	 chrome.tabs.insertCSS(null, {
+        code:
+        ".grid-overlay-container {"
+		  + "pointer-events: none;"
+		  + "box-sizing: border-box;"
+		  + "height: 100%;"
+		  + "margin: 0 auto;"
+		  + "padding: 0 4px;"
+		  + "max-width:" + options.width + "px;"
+		  + "position: relative;"
+		  + "background: rgba(99,99,99,0.2);"
+		+ "}"
+    });
+
+}
+
+function executeJS(){
+	chrome.tabs.executeScript(null, {file: 'grid.js'}); 
+}
 
 document.getElementById('addGrid').addEventListener('click', addGrid);
-document.getElementById('removegrid').addEventListener('click', addGrid);
+document.getElementById('removegrid').addEventListener('click', removeGrid);
+document.getElementById('updategrid').addEventListener('click', upDateGrid);
