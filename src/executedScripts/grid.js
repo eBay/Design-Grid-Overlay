0268b1d443e8bb16b7d6c62d7dd095494ff4c3bb
Norman <chnorman@lm-sjc-11000325.corp.ebay.com>
2016-03-25 14:27:52 -0700

    Updates to the manifest.json

--- src/executedScripts/grid.js
+++ src/executedScripts/grid.js
@@ -1,40 +1,40 @@
-//var chrome = chrome || {}; //Don't think we need this
+chrome.runtime.onMessage.addListener(
+ function(request, sender, sendResponse) {
+        if(request.method == "create"){
+            chrome.storage.sync.get(request.tabId.toString(), function(item) {
+            var numColumns = item[request.tabId.toString()].largeColumns || 16;       
 
-if (document.getElementsByClassName('cb-grid-lines').length) {
-    document.body.removeChild(document.getElementsByClassName('cb-grid-lines')[0]);
-    respond(0);
-} else {
-    console.log(chrome.tabs);
-    chrome.storage.sync.get(["largeColumns"], function(item) {
-        respond(1);
-        var numColumns = item.largeColumns || 16;       
+            var output = '<div class="cb-grid-lines"> \
+              <div class="grid-overlay-container"> \
+                <div class="grid-overlay-row">';
 
-        var output = '<div class="cb-grid-lines"> \
-          <div class="grid-overlay-container"> \
-            <div class="grid-overlay-row">';
+            for (var i = 0; i < numColumns; i += 1) {
+                output +=  '<div class="grid-overlay-col"></div>';
+            }
 
-        for (var i = 0; i < numColumns; i += 1) {
-            output +=  '<div class="grid-overlay-col"></div>';
+            output += '</div> \
+              </div> \
+            </div>';
+
+            document.body.innerHTML += output;
+
+            respond(1);
+        });
+    }
+});
+
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse){
+        if(request.method == "destroy" && document.getElementsByClassName('cb-grid-lines').length){
+            document.body.removeChild(document.getElementsByClassName('cb-grid-lines')[0]);
+            respond(0);
         }
+});
 
-        output += '</div> \
-          </div> \
-        </div>';
-
-        document.body.innerHTML += output;
-
-        //var columnSelector = '.grid-overlay-container .row > .span1';
-        //var gridOverlay = document.querySelectorAll('.grid-overlay-container');
-        //var columns = document.querySelectorAll(columnSelector);
-    });
-}
 
 function respond(gridStatus) {
     chrome.runtime.sendMessage({status: gridStatus}, function(response) {
       //console.log(response);
     });
 }
-
-window.addEventListener("scroll", function(){
-    console.log('scroll');
-});
